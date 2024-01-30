583f45b472a4ea5adb8142fc7ec871f87f2b4a17
NguyenMinhChau <nmchauhcmue@gmail.com>
2024-01-30 09:41:59 +0700

    push code

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	distDir: 'production',
 	basePath: '/github_actions',
 	output: 'export', // <=== enables static exports
 	reactStrictMode: true,
